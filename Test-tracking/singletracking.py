e906037429cbbb8162f203b2669042a5e64b8a96
lucas <l.h.g.oord@students.uu.nl>
2026-01-16 10:19:44 +0100

    test metingen

--- Test-tracking/singletracking.py
+++ Test-tracking/singletracking.py
@@ -42,7 +42,7 @@ directory = ''
 # Naam video
 #vidname = 'slinger.MOV' # Exilim
 #vidname = 'slinger.MP4' # GoPro
-vidname = 'testvid1.MOV' # Exilim
+vidname = 'testvid3.MOV' # Exilim
 
 # Directory om resultaten weg te schrijven 
 dir_write = ''
